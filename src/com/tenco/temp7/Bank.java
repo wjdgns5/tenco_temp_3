7bea756bb747d1ae0a427a8d7016dd83783ccffc
k_dy <ehddbs1105@gmail.com>
2024-04-18 16:15:12 +0900

    Bank 임시 파일

---
+++ src/com/tenco/temp7/Bank.java
@@ -0,0 +1,35 @@
+package com.tenco.temp7;
+
+import java.util.Scanner;
+
+public class Bank {
+
+	static Scanner sc = new Scanner(System.in);
+	static UserInfo[] userInfo = new UserInfo[10];
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		for (int i = 0; i < 10; i++) {
+			
+			userInfo[i] = new UserInfo(i);
+		}
+		
+		choice1();// 돈을 쓸지 저금을 할지 선택
+		
+		
+		
+	}
+	
+	
+	static void choice1(){
+		boolean flag = true;
+		while(flag)
+		System.out.println("1. 소비 2. 저금 | 선택 ㄱ ㄱ");
+		sc.nextInt();
+		
+	}
+
+}

e98700a5fc3e535975c09c2373288258dcdab354
wjdgns5 <rhfovkeh5@naver.com>
2024-04-18 17:34:27 +0900

    ..

---
+++ src/com/tenco/temp7/UserInfo.java
@@ -0,0 +1,35 @@
+package com.tenco.temp7;
+
+public class UserInfo {
+	// 매개 변수
+	
+	private int id; // 아이디
+	private String gender; // 성별
+	private int number; // 번호
+	
+	// 생성자
+	public UserInfo(int id) {
+		this.id = id;
+	}
+	
+	// GetterId
+	public int GetId() {
+		return id;
+	}
+	
+	// Setter
+	public void SetId(int id) {
+		this.id = id;
+	}
+	
+	public String GetName() {
+		return gender;
+	}
+	
+	
+	
+	
+	
+	
+
+}

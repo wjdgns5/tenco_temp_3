ab92de1af00f5d5794813cb7eaf29e1fd413aecb
kdt4-mh <alsgur6811@gmail.com>
2024-04-18 17:45:31 +0900

    .

--- src/com/tenco/temp7/UserTrack.java
+++ src/com/tenco/temp7/UserTrack.java
@@ -1,17 +1,23 @@
 package com.tenco.temp7;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class UserTrack {
 
 	private String name;
 
-	public void userTrack(String name) {
+	public void userTrack(int i) {
+		Bank bank = new Bank();
+		String name;
+		name = Bank.userInfo[i].GetName;
 		UserInfo userInfo = new UserInfo();
 		this.name = userInfo.getName;
 		boolean flag = this.name.equals(name); // 방어적 코드
 		if (flag == true) {
-			System.out.println("이름 : " + userInfo.getName);
-			System.out.println("계좌 번호 : " + userInfo.getId);
-			System.out.println("잔액 : " + userInfo.getMoney);
+			System.out.println("이름 : " + userInfo[i].GetName);
+			System.out.println("계좌 번호 : " + userInfo.GetId);
+			UserAccount.showInfo();
 		} else {
 			System.out.println("잘못된 입력입니다.");
 		}

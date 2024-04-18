2d5c85e10cb8adf8614678d050f2fdf805850d6e
kdt4-mh <alsgur6811@gmail.com>
2024-04-18 17:28:40 +0900

    user track v2

--- src/com/tenco/temp7/UserTrack.java
+++ src/com/tenco/temp7/UserTrack.java
@@ -1,19 +1,17 @@
 package com.tenco.temp7;
 
-import java.util.Set;
-
 public class UserTrack {
 
 	private String name;
-	private String userId;
-	private int balance;
-	
-	public void userTrack(String name) { 
+
+	public void userTrack(String name) {
+		UserInfo userInfo = new UserInfo();
+		this.name = userInfo.getName;
 		boolean flag = this.name.equals(name); // 방어적 코드
 		if (flag == true) {
 			System.out.println("이름 : " + userInfo.getName);
 			System.out.println("계좌 번호 : " + userInfo.getId);
-			System.out.println("잔액 : " + userInfo.getMoney); 
+			System.out.println("잔액 : " + userInfo.getMoney);
 		} else {
 			System.out.println("잘못된 입력입니다.");
 		}
